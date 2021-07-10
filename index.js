a3f4b04ea6c1759e879de14bb3e51a416999f160
teddy9264 <45321696+teddy9264@users.noreply.github.com>
2021-07-10 19:47:24 +1000

    Update index.js

--- index.js
+++ index.js
@@ -33,14 +33,33 @@ function requireHTTPS(req, res, next) {
   next();
 }
 
-'use strict';
-
-var con = mysql.createConnection({
+var sqlCredentials = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DB
-});
+};
+
+function handleDisconnect() {
+  con = mysql.createConnection(sqlCredentials); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  con.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
 
 const HMS_CRED = process.env.HMS_USER + " " + process.env.HMS_PASS + " " + process.env.HMS_DOMAIN
 
@@ -83,7 +102,9 @@ app.get('/', function(req, res) {
 			}
 		})
 	} else {
-		res.render('login');
+		res.render('login', {
+			server: { name: process.env.SERVER_NAME }
+		});
 	}
 });
 
